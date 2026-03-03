c3c981d117272234eef3a9716978eda1a9b92799
Jhonatan Muniz <jhonatanmuniz123@gmail.com>
2026-03-02 21:23:54 -0300

    feat: add fastify swagger generation

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import Fastify from "fastify";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifySwagger from "@fastify/swagger";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
@@ -9,6 +12,18 @@ const app = Fastify({ logger: true });
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: { title: "Training Planning API", version: "1.0.0" },
+    servers: [{ url: "http://localhost:3333" }],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
 
 app.get("/", async (request, reply) => {
   return { hello: "world" };
